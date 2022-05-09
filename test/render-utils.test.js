5d11922c40a421339b33fc60e13951f23f5c7a3a
sirdan <stanford303@gmail.com>
2022-05-09 16:14:02 -0700

    fixxed errors

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -5,10 +5,10 @@ import { renderListItem } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test.skip('renderListItem renders a div with animal details', (expect) => {
+test('renderListItem renders a div with animal details', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="animal" style="top: 225px; left: 20%;"><img src="./assets/sheep.svg"><span class="name">Samuel the Sheep</span><span>Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.</span></div>`;
+    const expected = `<div class="animal" style="top: 225px; left: 20%;"><img src="./assets/sheep.svg"><span class="name">Samuel the Sheep</span><span>Just because we're sheep, it doesn't mean we have to follow. Think for yourselves.</span><a href="./farAnimal/?id=undefined">More Info</a></div>`;
 
     //Act
     // Call the function you're testing and set the result to a const
